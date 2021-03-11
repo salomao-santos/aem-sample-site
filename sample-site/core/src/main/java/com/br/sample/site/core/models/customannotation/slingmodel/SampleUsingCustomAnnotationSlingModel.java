059e30fc9ee4d05de38a9aed862f4084b1c947d4
salomao-santos <salomaosantos777@gmail.com>
2021-03-11 17:45:22 -0300

    melhorias no código.

--- sample-site/core/src/main/java/com/br/sample/site/core/models/customannotation/slingmodel/SampleUsingCustomAnnotationSlingModel.java
+++ sample-site/core/src/main/java/com/br/sample/site/core/models/customannotation/slingmodel/SampleUsingCustomAnnotationSlingModel.java
@@ -1,22 +1,19 @@
-package com.br.sample.site.core.models.multifield.customannotation.slingmodel;
+package com.br.sample.site.core.models.customannotation.slingmodel;
 
-import java.util.ArrayList;
+
+
 import java.util.HashMap;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Required;
-import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-import com.br.sample.site.core.models.slingmodel.StateModel;
 import com.br.sample.site.core.provider.Multifield;
+
+import lombok.Getter;
 
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class SampleUsingCustomAnnotationSlingModel {
@@ -28,8 +25,10 @@ public class SampleUsingCustomAnnotationSlingModel {
 	//Ao utilizar a anotação @Required recomendo utilizar o @Default, para evitar erros.
 	//@Required
 	//@Default(values = "title")
+	@Getter
 	private String title;
 
+	@Getter
 	@ValueMapValue
 	@Default(values = "description")
 	private String description;
@@ -38,45 +37,10 @@ public class SampleUsingCustomAnnotationSlingModel {
 //	@ChildResource(name = "states")
 //	private List<Resource> resourceStates;
 //	private List<StateModel> states;
-	
+
+	@Getter
 	@Multifield(name = "states", values = {"jrc:name", "initials", "flag"})
-    private List<HashMap> listItems;
+    private List<HashMap> states;
 
 
-//	@PostConstruct
-//	protected void init() {
-//		states = new ArrayList<StateModel>();
-//
-//		if (resourceStates != null) {
-//			states = populatedModelState(states, resourceStates);
-//		}
-//
-//	}
-
-	private List<StateModel> populatedModelState(List<StateModel> states, List<Resource> resources) {
-
-		for (Resource r : resources) {
-			StateModel stateModel = r.adaptTo(StateModel.class);
-			if (stateModel != null)
-				states.add(stateModel);
-		}
-
-		return states;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-//	public List<StateModel> getStates() {
-//		return states;
-//	}
-	
-	public List<HashMap> getListItems() {
-        return listItems;
-    }
 }
